977ca5dad8415590898c8126678eb81f7b8da529
iponkkkkk <bhaktiervann@gmail.com>
2026-02-15 02:04:16 +0700

    nutrilog-school-role

--- apps/api/src/utils/prisma.ts
+++ apps/api/src/utils/prisma.ts
@@ -1,13 +1,12 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import pkg from "pg";
+
 const { Pool } = pkg;
+
 import { PrismaClient } from "../generated/prisma/index.js";
 
-
-console.log("DATABASE_URL TERDETEKSI:", process.env.DATABASE_URL ? "YA" : "TIDAK");
-
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL 
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
 });
 const adapter = new PrismaPg(pool);
 
